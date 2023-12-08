d21c96082e6fa71468d03f4bb29d820478e4ec2d
Gustavo Cruz <gustavo.cruz.laura@gmail.com>
2023-12-08 17:20:51 -0400

    Adding some extra documentation

--- index.mjs
+++ index.mjs
@@ -6,8 +6,10 @@ const s1 = "5F3Z-2e-9-w";
 const k1 = 4;
 
 console.log(licenseFormatter(s1, k1));
+// expected 5F3Z-2E9W
 
 const s2 = "2-5g-3-J";
 const k2 = 2;
 
 console.log(licenseFormatter(s2, k2));
+// expected 2-5G-3J
